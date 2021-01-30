cd7b21c3774d1ce35827fc02484ea502ddf879bf
Chris-Huszcza <chris.huszcza@auftesting.co.uk>
2021-01-30 17:45:29 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mytestapplication/stocks/Stock.java
@@ -0,0 +1,66 @@
+package com.example.mytestapplication.stocks;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Stock {
+
+    private final String stockCode;
+    private static final String SHARE_PRICE_URL_PREFIX = "https://www.google.com/search?q=share+price+";
+    private static final String PRICE_SELECTOR = "span.IsqQVc.NprOob.XcVN5d";
+    private static final String PRICE_DIFF_SELECTOR = "span.WlRRw.IsqQVc";
+    private static final String DATE_TIME_SELECTOR = "span[jsname$=ihIZgd]";
+    private String price;
+    private String priceDiff;
+    private String dateTime;
+
+    public Stock(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public void refreshStockData() throws IOException, Exception {
+        Document doc = Jsoup.connect(SHARE_PRICE_URL_PREFIX + this.stockCode).get();
+        setPrice(doc);
+        setPriceDiff(doc);
+        setDateTime(doc);
+    }
+
+    private void setPrice(Document doc) throws Exception {
+        this.price = getTextOfFirstElementBySelector(doc, PRICE_SELECTOR);
+    }
+
+    public String getPrice() {
+        return this.price;
+    }
+
+    private void setPriceDiff(Document doc) throws Exception {
+        this.priceDiff = getTextOfFirstElementBySelector(doc, PRICE_DIFF_SELECTOR);
+        this.priceDiff = this.priceDiff.substring(0, this.priceDiff.indexOf(" "));
+    }
+
+    public String getPriceDiff() {
+        return this.priceDiff;
+    }
+
+    private void setDateTime(Document doc) throws Exception {
+        this.dateTime = getTextOfFirstElementBySelector(doc, DATE_TIME_SELECTOR);
+        this.dateTime = this.dateTime.replace(" Â·", "");
+        this.dateTime = this.dateTime.replace(" ·", "");
+    }
+
+    public String getDateTime() {
+        return this.dateTime;
+    }
+
+    private String getTextOfFirstElementBySelector(Document doc, String selector) throws Exception {
+        Elements elements = doc.select(selector);
+        for (Element element : elements) {
+            return element.text();
+        }
+        throw new Exception("The selector [" + selector + "] doesn't match.");
+    }
+}
