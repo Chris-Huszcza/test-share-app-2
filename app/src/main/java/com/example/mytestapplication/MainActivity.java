ce049c25562a6c20a24e4bc602a0f1a0b611fc99
Chris-Huszcza <chris.huszcza@auftesting.co.uk>
2021-01-30 19:00:03 +0000

    Added chcek for wifi

--- app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.mytestapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView text = findViewById(R.id.textView);
+    Button refreshTextBtn = findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context applicationContext = getApplicationContext();
+
+        refreshTextBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if (WifiChecker.isWifiEnabled(applicationContext)) {
+                    text.setText("The text has changed.");
+                }
+                else {
+                    text.setText("Wifi is not enabled - doing nothing.");
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mytestapplication/WifiChecker.java
@@ -0,0 +1,25 @@
+package com.example.mytestapplication;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+
+public class WifiChecker {
+
+    public static boolean isWifiEnabled(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager.isWifiEnabled()) { // Wi-Fi adapter is ON
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo.getNetworkId() == -1){
+                return false; // Not connected to an access point
+            }
+            return true; // Connected to an access point
+        }
+        else {
+            return false; // Wi-Fi adapter is OFF
+        }
+    }
+}
