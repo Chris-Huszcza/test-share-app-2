2f3727ca0378f6507a399d594bdba6624fb472bb
Chris-Huszcza <chris.huszcza@auftesting.co.uk>
2021-01-31 14:40:28 +0000

    Added notifications

--- app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -1,28 +1,42 @@
 package com.example.mytestapplication;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mytestapplication.services.CheckStockService;
+import com.example.mytestapplication.stocks.Stock;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text = findViewById(R.id.textView);
-    Button refreshTextBtn = findViewById(R.id.button);
+    TextView text;
+    Button refreshTextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         Context applicationContext = getApplicationContext();
+
+        text = findViewById(R.id.textView);
+        refreshTextBtn = findViewById(R.id.button);
 
         refreshTextBtn.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
--- app/src/main/java/com/example/mytestapplication/services/CheckStockService.java
+++ app/src/main/java/com/example/mytestapplication/services/CheckStockService.java
@@ -1,5 +1,8 @@
 package com.example.mytestapplication.services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -7,12 +10,64 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.example.mytestapplication.R;
+import com.example.mytestapplication.WifiChecker;
+import com.example.mytestapplication.stocks.Stock;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 public class CheckStockService extends Service {
+
+    private static final String CHANNEL_ID = "stock_channel_01";
+    private Stock mnzsStock = new Stock("MNZS");
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service started", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Starting Service", Toast.LENGTH_LONG).show();
+
+        if (checkSystemState()) {
+            String displayText;
+            try {
+                mnzsStock.refreshStockData();
+                displayText = mnzsStock.getPrice();
+            } catch (Exception e) {
+                displayText = "Exception: " + ExceptionUtils.getStackTrace(e);
+            }
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            String name = "Share price updates";
+            String description = "Changes to share prices";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+            //text.setText("MNZS price = " + displayText);
+            Notification notification = buildNotification(displayText);
+            notificationManager.notify(0, notification);
+        }
         return START_STICKY;
+
+    }
+
+    private Notification buildNotification(String text) {
+        return new Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle("MNZS Share Price")
+                .setContentText("MNZS Share Price: " + text)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .build();
+    }
+
+    private boolean checkSystemState() {
+        // Check wifi is on
+        if (!WifiChecker.isWifiEnabled(getApplicationContext())) {
+            Toast.makeText(this, "Wifi is not enabled, stopping service", Toast.LENGTH_LONG).show();
+            stopService(new Intent(getBaseContext(), CheckStockService.class));
+            return false;
+        }
+        //
+        return true;
     }
 
     @Nullable
@@ -25,6 +80,6 @@ public class CheckStockService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
     }
 }
